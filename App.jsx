840b1693feca1c64675dab69981780a2b54b7fea
Mesandu Gunarwardhana <mesandugunarwardhana@gmail.com>
2025-11-29 17:44:30 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import Movie from './components/moviesearcher.jsx'
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Movie/>
+
+     
+    </div>
+  )
+}
+
+export default App
